3c1dea204014d3f2f77a20bce742ce36a89636c5
negin <neginhd6@gmail.com>
2025-03-29 21:02:18 -0400

    updated app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import PropertyDetails from './screens/ListingComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <PropertyDetails />
+    </div>
+  );
+}
+
+export default App;
